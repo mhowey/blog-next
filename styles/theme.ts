b00c658ea9769d2b83b1b719713c1d2a2f992f35
Matt Howey <howey.matt@gmail.com>
2023-05-01 18:14:34 -0400

    Updating global styles so prefer-dark-mode has no effect

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,19 +1,26 @@
 import { createTheme } from '@mui/material/styles'
-import ListItemText from '@mui/material/ListItemText';
+
 export const theme = createTheme({
   components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: process.env.NEXT_PUBLIC_THEME_DARK,
+        },
+      },
+    },
     MuiPaper: {
       styleOverrides: {
         root: {
-          backgroundColor: process.env.NEXT_PUBLIC_THEME_DARK,
-          color: "white",
+          backgroundColor: process.env.NEXT_PUBLIC_THEME_PRIMARY,
+          color: process.env.NEXT_PUBLIC_THEME_DARK,
         },
       },
     },
     MuiAppBar: {
       styleOverrides: {
         root: {
-          backgroundColor: process.env.NEXT_PUBLIC_THEME_DARK,
+          backgroundColor: process.env.NEXT_PUBLIC_THEME_PRIMARY,
         },
       },
     }
@@ -23,13 +30,18 @@ export const theme = createTheme({
     h1: {
       fontSize: "2rem",
       marginBottom: "1rem",
+      color: process.env.NEXT_PUBLIC_THEME_LIGHT,
+    },
+    h6: {
+      color: process.env.NEXT_PUBLIC_THEME_TERTIARY,
     },
     body1: {
-      // marginBottom: "1.5rem",
+      color: process.env.NEXT_PUBLIC_THEME_DARK,
     },
     body2: {
       marginBottom: "1rem",
       fontSize: "1.2rem",
+      color: process.env.NEXT_PUBLIC_THEME_LIGHT,
     },
   },
   palette: {
